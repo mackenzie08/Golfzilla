94b581511cef3abad3222759ec64608f609de14c
mackenzie08 <56842664+mackenzie08@users.noreply.github.com>
2020-03-31 13:51:52 -0400

    Add files via upload

---
+++ golfzilla2.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+    <style>
+        .button3 {width: 50%;}
+    </style>
+<meta charset="utf-8">
+<title>Golfzilla</title>
+<link rel="stylesheet" href= "stylesheet.css">
+</head>
+
+
+
+<img src="golfzillalogo.png" alt="Logo"><br>
+<p>Welcome to Hole 1!</p> 
+<h2><label for="fname">Score:</label></h2>
+<div><button>1</button>
+<button>2</button>
+<button>3</button>
+<button>4</button>
+<button>5</button>
+<button>6</button>
+<button>7</button>
+<button>8</button>
+<button>9</button>
+<button>10</button></div> <br>
+<p>
+<button onclick="document.location = 'golfzilla.php'">Return to Main Page</button><br>
+ </p>   
+   
+     
+</div>
+
+
+
+<div class="container">
+    <p id="response"></p>
+    
+	
+</div>
+
+
+
+</body>
+</html>
